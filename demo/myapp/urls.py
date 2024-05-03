be04d09ee8ac73e2d413e17df8728f54abda6207
davifelipef <feliperedfield@gmail.com>
2024-05-02 23:11:30 -0300

    Standard url path convention adopted

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("importar_ata.html", views.importar_ata, name="importar_ata"),
+    path("", views.home, name="inicio"),
+    path("importar_ata/", views.importar_ata, name="importar_ata"),
     path("todos/", views.todos, name="Todos")
 ]
